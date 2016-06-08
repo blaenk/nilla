4909a44fe30fdf92a38373d4bfe68f15c88e9e4b
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-06-08 16:44:11 -0700

    test settings

---
+++ test/test-init.js
@@ -0,0 +1,17 @@
+"use strict";
+
+global.chai = require('chai');
+
+global.chaiAsPromised = require('chai-as-promised');
+global.chai.use(global.chaiAsPromised);
+
+global.sinon = require('sinon');
+global.sinonChai = require('sinon-chai');
+global.chai.use(global.sinonChai);
+
+global.chai.should();
+global.chaiAsPromised = global.chaiAsPromised;
+global.expect = global.chai.expect;
+global.AssertionError = global.chai.AssertionError;
+global.Assertion = global.chai.Assertion;
+global.assert = global.chai.assert;
