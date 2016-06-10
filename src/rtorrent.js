a9310e57df6b5d9ee5ac364664884043280ae2a9
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-06-09 20:18:31 -0700

    preliminary rtorrent.js tests

--- src/rtorrent.js
+++ src/rtorrent.js
@@ -1,11 +1,17 @@
 "use strict";
 
-var process = require('process');
-
 var Bluebird = require('bluebird');
-var xmlrpc = require('xmlrpc');
 
+var process = require('process');
+var path = require('path');
+var fs = Bluebird.promisifyAll(require('fs'));
+
+var xmlrpc = require('xmlrpc');
 var SCGITransport = require('./xmlrpc_scgi.js');
+
+var bencode = require('bencode');
+var magnet = require('magnet-uri');
+var crypto = require('crypto');
 
 const call = (method, args, opts) => {
   opts = Object.assign({
@@ -64,9 +70,76 @@ const torrents = (view, ...args) => {
   return call('d.multicall', [view, ...args]);
 };
 
+const infoHash = (buffer) => {
+  let decodedBuffer = bencode.decode(buffer);
+  let encodedInfo = bencode.encode(decodedBuffer["info"]);
+
+  return crypto.createHash('sha1').update(encodedInfo).digest('hex');
+};
+
+const disableFilesForNonStarted = (startNow, commands) => {
+  if (!startNow) {
+    return ["f.multicall=,,f.set_priority=0", "d.update_priorities="].concat(commands);
+  }
+
+  return commands;
+};
+
+// commands:
+//
+// load.normal
+// load.start
+// load.raw
+// load.raw_start
+
+// Load a .torrent file
+const loadFile = (filePath, commands, startNow, loadRaw, opts) => {
+  commands = disableFilesForNonStarted(startNow, commands || []);
+
+  return fs.readFileAsync(filePath)
+    .then(buffer => {
+      let infohash = infoHash(buffer);
+      let method = 'load';
+
+      if (loadRaw) {
+        method += '_raw';
+      }
+
+      if (startNow) {
+        method += '_start';
+      }
+
+      let args = [loadRaw ? buffer : path.resolve(filePath)].concat(commands);
+
+      return call(method, args, opts).then(() => Bluebird.resolve(infohash));
+    });
+};
+
+const loadMagnet = (uri, startNow, commands, opts) => {
+  commands = disableFilesForNonStarted(startNow, commands || []);
+  let method = startNow ? "load_start" : "load";
+
+  var parsed = magnet.decode(uri);
+  var infohash = parsed.infoHash;
+
+  return call(method, [uri], commands, opts).then(() => Bluebird.resolve(infohash));
+};
+
+const setTiedFile = (filepath) => call('d.tied_to_file.set', [filepath]);
+
+const removeTorrent = (infohash) => {
+  return call('d.erase', [infohash]);
+};
+
 module.exports = {
   call,
   multicall,
   torrent,
-  torrents
+  torrents,
+
+  loadFile,
+  loadMagnet,
+
+  setTiedFile,
+  removeTorrent
 };
--- test/rtorrent.js
+++ test/rtorrent.js
@@ -1,26 +1,102 @@
 "use strict";
 
+var Bluebird = require('bluebird');
+
 var rtorrent = require('../src/rtorrent.js');
+
+const torrents = {
+  arch: {
+    path: 'magnet:?xt=urn:btih:01000e92d5c8cf2473e5978b445de9624c04d11a&dn=archlinux-2016.06.01-dual.iso&tr=udp://tracker.archlinux.org:6969',
+    hash: '01000e92d5c8cf2473e5978b445de9624c04d11a'
+  },
+  ubuntu: {
+    name: 'ubuntu-16.04-desktop-amd64.iso',
+    path: './test/fixtures/ubuntu-16.04-desktop-amd64.iso.torrent',
+    hash: '4344503b7e797ebf31582327a5baae35b11bda01'
+  },
+  fedora: {
+    path: './test/fixtures/Fedora-Live-Workstation-x86_64-23.torrent',
+    hash: '796ab93bb81e2dbe072f3c07857675ee5c47b046'
+  }
+};
 
 describe('RTorrent', function() {
   before("should ensure a connection", function() {
-    return rtorrent.call("system.client_version", []);
+    // TODO
+    // should perform some isolation?
+    //
+    // * session.path.set to set the session directory?
+    // * directory.default.set to set download dir?
+
+    return Bluebird.all([
+      rtorrent.call("system.client_version", [])
+        .should.eventually.equal("0.9.6"),
+
+      rtorrent.loadFile(torrents.ubuntu.path, [], false, true)
+        .should.eventually.equal(torrents.ubuntu.hash),
+
+      rtorrent.loadMagnet(torrents.arch.path)
+        .should.eventually.equal(torrents.arch.hash)
+    ]);
   });
 
-  context('add and remove torrents', function() {
-    it('add a torrent from a file');
-    it('remove a torrent');
+  context.skip('add and remove torrents', function() {
+    it('add a torrent from a file', function() {
+      return rtorrent.loadFile(torrents.fedora.path)
+        .should.eventually.equal(torrents.fedora.hash);
+    });
 
-    it('add a torrent from raw data');
-    it('add a torrent from a magnet link');
+    it('add a torrent from a magnet link', function() {
+      return rtorrent.loadMagnet(torrents.arch.path)
+        .should.eventually.equal(torrents.arch.hash);
+    });
   });
 
-  context('manage file properties', function() {
-    it('set a file priority to 0');
+  context('manipulate torrents', function() {
+    it('read the name', function() {
+      return rtorrent.call('d.get_name', [torrents.ubuntu.hash]).
+        should.eventually.equal(torrents.ubuntu.name);
+    });
+
+    context('manage file properties', function() {
+      it('set a file priority to 0');
+    });
+
+    context('set custom metadata', function() {
+      it('should set "name" to "bob"');
+      it('should read "name" as "bob"');
+    });
   });
 
-  context('set custom metadata', function() {
-    it('should set "name" to "bob"');
-    it('should read "name" as "bob"');
+  after('remove torrents', function() {
+    return Bluebird.map([
+      torrents.ubuntu.hash,
+      torrents.arch.hash
+    ], rtorrent.removeTorrent);
   });
 });
+
+// NOTE
+//
+// * Inserting metadata on download insertion creates the potential for rtorrent
+//   crashing. It seems like if a torrent is added and removed and the same is
+//   repeated in quick succession, a race condition (?) can occur which crashes
+//   rtorrent.
+//
+// * Adding a torrent by file to rtorrent saves information about it in a cache,
+//   in particular the file's path and mtime.
+//
+//   see src/core/manager.cc Manager::try_create_download()
+//
+//   The implication of this is that an added file can't be re-added unless
+//   rtorrent is restarted. There's a command for pruning this "cache" but an
+//   entry is only pruned if the .torrent file no longer exists or the file's
+//   mtime is newer than that recorded in the cache.
+//
+//   see src/utils/file_status_cache.cc FileStatusCache::prune()
+//
+//   The command is: system.file_status_cache.prune
+//   There's also: system.file_status_cache.size
+//
+//   Finally there's also a built-in scheduled job which runs the prune command
+//   daily beginning an hour after rtorrent is launched.
