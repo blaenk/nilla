bfa0b3aaffcda5ea623fa603d2dd184600dec214
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-06-15 13:14:54 -0700

    hjs script runs dev server

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,34 @@
 const webpack = require('webpack');
 const fs = require('fs');
 const path = require('path');
-const getConfig = require('hjs-webpack');
 
-const root = path.resolve(__dirname);
-const src = path.join(root, 'src');
-const modules = path.join(root, 'node_modules');
-const dest = path.join(root, 'dist');
+const getConfig = require('hjs-webpack');
 
 const NODE_ENV = process.env.NODE_ENV;
 const isDev = NODE_ENV === 'development';
 
+const root = path.resolve(__dirname);
+const src = path.join(root, 'src');
+// const modules = path.join(root, 'node_modules');
+const dest = path.join(root, 'dist');
+
 const config = getConfig({
   isDev,
-  "in": path.join(src, 'src/app.js'),
-  "out": dest,
-  clearBeforeBuild: true
+  "in": path.join(src, 'app.js'),
+  out: dest,
+  clearBeforeBuild: true,
+  devServer: {
+    port: 4000,
+    hostname: "0.0.0.0"
+  },
+  modules: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      }
+    ]
+  }
 });
 
 module.exports = config;
