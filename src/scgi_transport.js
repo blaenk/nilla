9da313b945ecd375dced1989b89669857842e74e
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-06-14 21:56:59 -0700

    remove dead code

--- src/scgi_transport.js
+++ src/scgi_transport.js
@@ -4,32 +4,7 @@ var stream = require('stream');
 var util = require('util');
 var events = require('events');
 
-var Client = require('xmlrpc/lib/client');
-var Serializer = require('xmlrpc/lib/serializer');
-var Deserializer = require('xmlrpc/lib/deserializer');
-
 var scgi = require('./scgi');
-
-var Bluebird = require('bluebird');
-
-Bluebird.promisifyAll(Deserializer.prototype);
-
-Client.prototype.scgiMethodCall = function scgiMethodCall(method, params) {
-  var options = this.options;
-  var xml     = Serializer.serializeMethodCall(method, params, options.encoding);
-
-  return scgi.request(options, xml)
-    .then((response) => {
-      var deserializer = new Deserializer(options.responseEncoding);
-
-      var s = new stream.Readable();
-      s._read = () => {};
-      s.push(response.body);
-      s.push(null);
-
-      return deserializer.deserializeMethodResponseAsync(s);
-    });
-};
 
 class SCGITransport {
   static request(options, callback) {
