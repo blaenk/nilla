1075a4296aeb45d17b7ca14eb8b30cbc08c3b8ef
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-06-30 13:43:22 -0700

    make otherwise-literal patterns fuzzy

--- client/src/common/util.js
+++ client/src/common/util.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
 
+/**
+ * Construct a "fuzzy" regular expression from the input string. If the input
+ * string is an invalid regular expression, escape it first.
+ * @param {String} literalPattern The input string.
+ * @returns {RegExp} The fuzzy regular expression.
+ */
 export function fuzzyPattern(literalPattern) {
   try {
     return new RegExp(literalPattern.replace(/ /g, '.*'), 'i');
   } catch (e) {
     const escaped = _.escapeRegExp(literalPattern);
-    return new RegExp(escaped);
+    return new RegExp(escaped.replace(/ /g, '.*', 'i'));
   }
 }
