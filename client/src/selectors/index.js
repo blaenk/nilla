83205909c28a40b4312f4c2fab4d8624cbdecfc3
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-06-30 16:22:55 -0700

    preliminary handling of scope changing

--- client/src/selectors/index.js
+++ client/src/selectors/index.js
@@ -1,5 +1,6 @@
 import { createSelector } from 'reselect';
 import { fuzzyPattern } from 'common';
+import moment from 'moment';
 
 const getDownloads = (state) => state.downloads;
 const getFilter = (state) => state.search.filter;
@@ -9,6 +10,31 @@ export const getScopedDownloads = createSelector(
   [getDownloads, getScope],
   (downloads, scope) => {
     switch (scope) {
+      case 'mine':
+        return downloads.filter(download => {
+          // TODO
+          // get username from auth
+          return download.uploader == 'blaenk';
+        });
+      case 'system':
+        return downloads.filter(download => {
+          return download.uploader == 'system';
+        });
+      case 'locked':
+        return downloads.filter(download => {
+          return download.locks.length > 0;
+        });
+      case 'expiring':
+        // get downloads expiring within the next 24 hours
+        return downloads.filter(download => {
+          // TODO
+          // don't hard-code expiration time
+          // perhaps store some TTL in metadata?
+          const expiresAt =
+                moment(download['date-added']).add(2, 'weeks').subtract(1, 'day');
+
+          return moment().isSameOrAfter(expiresAt);
+        });
       case 'all':
       default:
         return downloads;
