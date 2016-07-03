fb6316c854c2d018658c69bfdd8f4a6bef9b4f07
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-07-02 19:56:36 -0700

    create file rejection error component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import {
   Collapse,
-  Grid,
-  Table
+  Grid
 } from 'react-bootstrap';
 import CSSModules from 'react-css-modules';
 import Dropzone from 'react-dropzone';
-import filesize from 'filesize';
 
 import Header from './Header';
 import Upload from './Upload';
-import ErrorAlert from './ErrorAlert';
 
 import styles from 'styles/app.module.less';
 
@@ -39,6 +36,7 @@ const App = React.createClass({
   onDropRejected: function(files) {
     this.setState({
       isDragging: false,
+      isUploading: true,
       rejectedFiles: files
     });
   },
@@ -48,48 +46,6 @@ const App = React.createClass({
   },
 
   render: function() {
-    // TODO
-    // create a collection of errors that can be shown
-
-    let rejectionError;
-
-    if (this.state.rejectedFiles) {
-      let files = this.state.rejectedFiles.map(file => {
-        return (
-          <tr key={file.name}>
-            <td>{file.name}</td>
-            <td>{filesize(file.size)}</td>
-            <td>{file.type || 'unknown'}</td>
-          </tr>
-        );
-      });
-
-      rejectionError = (
-        <ErrorAlert title="Unrecognized File!"
-                    onDismiss={this.dismissRejectionAlert}>
-          <p>
-            One or more of the files you chose is not a torrent! Please try
-            again without the offending files.
-          </p>
-
-          <p>Here are the files you attempted to upload:</p>
-
-          <Table striped responsive styleName='rejected-files'>
-            <thead>
-              <tr>
-                <th>name</th>
-                <th>size</th>
-                <th>type</th>
-              </tr>
-            </thead>
-            <tbody>
-              {files}
-            </tbody>
-          </Table>
-        </ErrorAlert>
-      );
-    }
-
     return (
       <Dropzone onDropAccepted={this.onDropAccepted}
                 onDropRejected={this.onDropRejected}
@@ -103,11 +59,11 @@ const App = React.createClass({
           <Header onUpload={() => this.setState({isUploading: !this.state.isUploading})}
                   isDragging={this.state.isDragging} />
 
-          {rejectionError}
-
           <Collapse in={this.state.isUploading}>
             <div>
-              <Upload onClickFiles={() => this.refs.dropzone.open()}
+              <Upload rejectedFiles={this.state.rejectedFiles}
+                      onDismissRejectionAlert={this.dismissRejectionAlert}
+                      onClickFiles={() => this.refs.dropzone.open()}
                       onClickUpload={() => console.log('clicked upload')}
                       onSubmitMagnet={() => console.log('submit magnet')} />
             </div>
--- client/src/components/App/Upload.js
+++ client/src/components/App/Upload.js
@@ -5,22 +5,79 @@ import {
   Col,
   FormControl,
   InputGroup,
-  Row
+  Row,
+  Table
 } from 'react-bootstrap';
 import CSSModules from 'react-css-modules';
+import filesize from 'filesize';
 
 import styles from './upload.module.less';
 
+import ErrorAlert from './ErrorAlert';
+
+const RejectedFilesErrorAlert = React.createClass({
+  propTypes: {
+    rejectedFiles: React.PropTypes.array,
+    onDismiss: React.PropTypes.func.isRequired
+  },
+
+  render: function() {
+    if (this.props.rejectedFiles) {
+      let files = this.props.rejectedFiles.map(file => {
+        return (
+          <tr key={file.name}>
+            <td>{file.name}</td>
+            <td>{filesize(file.size)}</td>
+            <td>{file.type || 'unknown'}</td>
+          </tr>
+        );
+      });
+
+      return (
+        <ErrorAlert title="Unrecognized File!"
+                    onDismiss={this.props.onDismiss}>
+          <p>
+            One or more of the files you chose is not a torrent! Please try
+            again without the offending files.
+          </p>
+
+          <p>Here are the files you attempted to upload:</p>
+
+          <Table striped responsive>
+            <thead>
+              <tr>
+                <th>name</th>
+                <th>size</th>
+                <th>type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {files}
+            </tbody>
+          </Table>
+        </ErrorAlert>
+      );
+    }
+
+    return null;
+  }
+});
+
 const Upload = React.createClass({
   propTypes: {
+    rejectedFiles: React.PropTypes.array,
     onSubmitMagnet: React.PropTypes.func.isRequired,
     onClickFiles: React.PropTypes.func.isRequired,
-    onClickUpload: React.PropTypes.func.isRequired
+    onClickUpload: React.PropTypes.func.isRequired,
+    onDismissRejectionAlert: React.PropTypes.func.isRequired
   },
 
   render: function() {
     return (
       <div>
+        <RejectedFilesErrorAlert rejectedFiles={this.props.rejectedFiles}
+                                 onDismiss={this.props.onDismissRejectionAlert} />
+
         <Row styleName='upload'>
           <Col lg={12}>
             <InputGroup>
