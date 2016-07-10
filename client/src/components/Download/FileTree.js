c80996c862c3353e7373b805381f79ebc11b91cd
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-07-10 01:06:57 -0700

    param separators

--- client/src/components/Download/FileTree.js
+++ client/src/components/Download/FileTree.js
@@ -9,14 +9,14 @@ import styles from './filetree.module.less';
 
 /**
  * Partitions a list of file objects into folders and files.
- * @param {Object[]} entries The list of file objects.
- * @param {string[]} entries[].pathComponents The file's path components.
- * @param {number} entries[].size The file's size.
- * @param {string} entries[].progress The file's download progress.
- * @param {number} entries[].id The file's rtorrent identifier.
- * @param {bool} entries[].enabled Whether the file is enabled.
- * @param {number} depth The current tree depth.
- * @returns {Array} The first element is an array of folders, second is array of
+ * @param {Object[]} entries - The list of file objects.
+ * @param {string[]} entries[].pathComponents - The file's path components.
+ * @param {number} entries[].size - The file's size.
+ * @param {string} entries[].progress - The file's download progress.
+ * @param {number} entries[].id - The file's rtorrent identifier.
+ * @param {bool} entries[].enabled - Whether the file is enabled.
+ * @param {number} depth - The current tree depth.
+ * @returns {Array} - The first element is an array of folders, second is array of
  * files.
  */
 function partitionFiles(entries, depth) {
