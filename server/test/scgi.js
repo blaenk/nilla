a771672c097f32bd6c531ab789930634dae5b990
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-07-22 15:35:18 -0700

    update for styling

--- server/test/scgi.js
+++ server/test/scgi.js
@@ -6,32 +6,32 @@ const scgi = require('../src/scgi.js');
 // examples taken from scgi protocol specification
 // https://python.ca/scgi/protocol.txt
 
-describe("SCGI", function() {
-  context("Protocol", function() {
+describe('SCGI', function() {
+  context('Protocol', function() {
     it('should build a request', () => {
       const request = scgi.buildRequest({
-        REQUEST_METHOD: "POST",
-        REQUEST_URI: "/deepthought"
-      }, "What is the answer to life, the Universe and everything?");
+        REQUEST_METHOD: 'POST',
+        REQUEST_URI: '/deepthought'
+      }, 'What is the answer to life, the Universe and everything?');
 
       const raw =
-        "70:" +
-        "CONTENT_LENGTH" + '\x00' + "56" + '\x00' +
-        "SCGI" + '\x00' +  "1" + '\x00' +
-        "REQUEST_METHOD" + '\x00' +  "POST" + '\x00' +
-        "REQUEST_URI" + '\x00' +  "/deepthought" + '\x00' +
-        "," +
-        "What is the answer to life, the Universe and everything?";
+        '70:' +
+        'CONTENT_LENGTH' + '\x00' + '56' + '\x00' +
+        'SCGI' + '\x00' +  '1' + '\x00' +
+        'REQUEST_METHOD' + '\x00' +  'POST' + '\x00' +
+        'REQUEST_URI' + '\x00' +  '/deepthought' + '\x00' +
+        ',' +
+        'What is the answer to life, the Universe and everything?';
 
       expect(raw).to.equal(request);
     });
 
     it('should parse a response', () => {
       const response =
-        "Status: 200 OK\r\n" +
-        "Content-Type: text/plain\r\n" +
-        "\r\n" +
-        "42";
+        'Status: 200 OK\r\n' +
+        'Content-Type: text/plain\r\n' +
+        '\r\n' +
+        '42';
 
       const parsed = scgi.parseResponse(response);
 
@@ -41,9 +41,9 @@ describe("SCGI", function() {
     });
   });
 
-  context("Transport", function() {
-    it("should establish a connection", function(done) {
-      const client = xmlrpc.createClient({path: process.env.RTORRENT_SOCKET});
+  context('Transport', function() {
+    it('should establish a connection', function(done) {
+      const client = xmlrpc.createClient({ path: process.env.RTORRENT_SOCKET });
 
       client.methodCallWithTransport(scgi.Transport, 'system.client_version', [], done);
     });
