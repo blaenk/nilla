f79d7890cf68e7166272b5f30dd43168b48755eb
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-08-01 22:01:44 -0700

    create action constants

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,8 @@
 import request from 'superagent';
 import Cookies from 'js-cookie';
 
+export const SET_SCOPE = 'SET_SCOPE';
+
 /**
  * Action to set the scope.
  * @param {string} scope The scope to set to. One of 'all', 'mine', 'system',
@@ -9,10 +11,12 @@ import Cookies from 'js-cookie';
  */
 export function setScope(scope) {
   return {
-    type: 'SET_SCOPE',
+    type: SET_SCOPE,
     scope,
   };
 }
+
+export const SET_ORDER = 'SET_ORDER';
 
 /**
  * Action to set the sort order.
@@ -21,10 +25,12 @@ export function setScope(scope) {
  */
 export function setOrder(order) {
   return {
-    type: 'SET_ORDER',
+    type: SET_ORDER,
     order,
   };
 }
+
+export const SET_DOWNLOADS_FILTER = 'SET_DOWNLOADS_FILTER';
 
 /**
  * Action to set the filter.
@@ -33,14 +39,16 @@ export function setOrder(order) {
  */
 export function setDownloadsFilter(filter) {
   return {
-    type: 'SET_DOWNLOADS_FILTER',
+    type: SET_DOWNLOADS_FILTER,
     filter,
   };
 }
 
+export const ADD_FILES = 'ADD_FILES';
+
 export function addFiles(files) {
   return {
-    type: 'ADD_FILES',
+    type: ADD_FILES,
     files: files.map(f => {
       return {
         start: true,
@@ -51,80 +59,102 @@ export function addFiles(files) {
   };
 }
 
+export const REMOVE_FILE = 'REMOVE_FILE';
+
 export function removeFile(file) {
   return {
-    type: 'REMOVE_FILE',
+    type: REMOVE_FILE,
     file,
   };
 }
 
+export const REJECT_FILE = 'REJECT_FILE';
+
 export function rejectFiles(files) {
   return {
-    type: 'REJECT_FILE',
+    type: REJECT_FILE,
     files,
   };
 }
 
+export const CLEAR_REJECTED_FILES = 'CLEAR_REJECTED_FILES';
+
 export function clearRejectedFiles() {
   return {
-    type: 'CLEAR_REJECTED_FILES',
+    type: CLEAR_REJECTED_FILES,
   };
 }
 
+export const SET_DRAGGING = 'SET_DRAGGING';
+
 export function setDragging(isDragging) {
   return {
-    type: 'SET_DRAGGING',
+    type: SET_DRAGGING,
     isDragging,
   };
 }
 
+export const SET_UPLOADING = 'SET_UPLOADING';
+
 export function setUploading(isUploading) {
   return {
-    type: 'SET_UPLOADING',
+    type: SET_UPLOADING,
     isUploading,
   };
 }
 
+export const UPLOAD_FILE = 'UPLOAD_FILE';
+
 export function uploadFile(file) {
   return {
-    type: 'UPLOAD_FILE',
+    type: UPLOAD_FILE,
     file,
   };
 }
 
+export const SET_FILE_START = 'SET_FILE_START';
+
 export function setFileStart(file, start) {
   return {
-    type: 'SET_FILE_START',
+    type: SET_FILE_START,
     file,
     start,
   };
 }
 
+export const SET_FILE_PROGRESS = 'SET_FILE_PROGRESS';
+
 export function setFileProgress(file, progress) {
   return {
-    type: 'SET_FILE_PROGRESS',
+    type: SET_FILE_PROGRESS,
     file,
     progress,
   };
 }
 
+export const REQUEST_DOWNLOADS = 'REQUEST_DOWNLOADS';
+
 export function requestDownloads() {
   return {
-    type: 'REQUEST_DOWNLOADS',
+    type: REQUEST_DOWNLOADS,
   };
 }
 
+export const RECEIVE_DOWNLOADS = 'RECEIVE_DOWNLOADS';
+
 export function receiveDownloads(downloads) {
   return {
-    type: 'RECEIVE_DOWNLOADS',
+    type: RECEIVE_DOWNLOADS,
     downloads,
     receivedAt: Date.now(),
   };
 }
 
+export const RECEIVE_DOWNLOAD = 'RECEIVE_DOWNLOAD';
+
 export function receiveDownload(download) {
   return {
-    type: 'RECEIVE_DOWNLOAD',
+    type: RECEIVE_DOWNLOAD,
     download,
     receivedAt: Date.now(),
   };
@@ -172,6 +202,10 @@ export function submitFile(file) {
       return Promise.reject(new Error(`file not found: ${file}`));
     }
 
+    const getFileObject = () => {
+      return getState().upload.files.find(f => f.backingFile === fileObject.backingFile);
+    };
+
     return request.post('/api/downloads')
       .accept('json')
       .set('X-CSRF-TOKEN', Cookies.get('csrf-token'))
@@ -179,19 +213,10 @@ export function submitFile(file) {
       .field('start', fileObject.start)
       .on('progress', event => {
         dispatch(setFileProgress(fileObject, event.percent));
-
-        fileObject = getState().upload.files.find(f => f.backingFile === fileObject.backingFile);
+        fileObject = getFileObject();
       })
-      .then(_response => {
-        dispatch(removeFile(fileObject));
-
-        fileObject = getState().upload.files.find(f => f.backingFile === fileObject.backingFile);
-      })
-      .catch(_error => {
-        dispatch(removeFile(fileObject));
-
-        fileObject = getState().upload.files.find(f => f.backingFile === fileObject.backingFile);
-      });
+      .then(_response => dispatch(removeFile(fileObject)))
+      .catch(_error => dispatch(removeFile(fileObject)));
   };
 }
 
