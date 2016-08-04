e7eb9dc6958e9a23d5c33382ac7185fafd30f744
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-08-04 13:51:48 -0700

    save app state to and load from localStorage

--- client/src/app.js
+++ client/src/app.js
@@ -7,7 +7,12 @@ import { browserHistory } from 'react-router';
 import AppContainer from 'containers/App/AppContainer';
 import Reducer from './reducers';
 
+const localState = window.localStorage.getItem('redux');
 let defaultState;
+
+if (localState !== null) {
+  defaultState = JSON.parse(localState);
+}
 
 let store = createStore(
   Reducer,
@@ -17,6 +22,18 @@ let store = createStore(
     window.devToolsExtension ? window.devToolsExtension() : f => f
   )
 );
+
+let currentState;
+
+store.subscribe(() => {
+  const previousState = currentState;
+
+  currentState = store.getState();
+
+  if (previousState !== currentState) {
+    window.localStorage.setItem('redux', JSON.stringify(currentState));
+  }
+});
 
 document.addEventListener('DOMContentLoaded', () => {
   const mountNode = document.querySelector('#root');
