b5db0ed1982f0adce15c543db9143e7a9a196e1a
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-09-07 14:20:11 -0700

    make Downloads/OrderDropDown a PureComponent

--- client/src/components/Downloads/OrderDropDown.js
+++ client/src/components/Downloads/OrderDropDown.js
@@ -11,30 +11,32 @@ import styles from './search.module.less';
 
 import MarkedMenuItem from './MarkedMenuItem';
 
-function OrderDropDown(props) {
-  const item = (name) => {
+class OrderDropDown extends React.PureComponent {
+  render() {
+    const item = (name) => {
+      return (
+        <MarkedMenuItem eventKey={name} selected={this.props.order}
+                        onSelect={this.props.onChangeOrder} key={name}>
+          {name}
+        </MarkedMenuItem>
+      );
+    };
+
     return (
-      <MarkedMenuItem eventKey={name} selected={props.order}
-                      onSelect={props.onChangeOrder} key={name}>
-        {name}
-      </MarkedMenuItem>
+      <InputGroup.Button>
+        <Dropdown id='sort_order'>
+          <Dropdown.Toggle noCaret styleName='sort'>
+            <Glyphicon glyph='sort' />
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu style={{ left: 'auto', right: 0 }}>
+            <MenuItem header>Sort By</MenuItem>
+            {['NAME', 'RECENT'].map(item)}
+          </Dropdown.Menu>
+        </Dropdown>
+      </InputGroup.Button>
     );
-  };
-
-  return (
-    <InputGroup.Button>
-      <Dropdown id='sort_order'>
-        <Dropdown.Toggle noCaret styleName='sort'>
-          <Glyphicon glyph='sort' />
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu style={{ left: 'auto', right: 0 }}>
-          <MenuItem header>Sort By</MenuItem>
-          {['NAME', 'RECENT'].map(item)}
-        </Dropdown.Menu>
-      </Dropdown>
-    </InputGroup.Button>
-  );
+  }
 }
 
 OrderDropDown.propTypes = {
