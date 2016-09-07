71216adcaa642091ebb56b5d9eb6416b754ed4d0
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-09-07 14:17:44 -0700

    make App/ErrorAlert a PureComponent

--- client/src/components/App/ErrorAlert.js
+++ client/src/components/App/ErrorAlert.js
@@ -4,28 +4,30 @@ import CSSModules from 'react-css-modules';
 
 import styles from './error.module.less';
 
-function ErrorAlert(props) {
-  return (
-    <Row>
-      <Col lg={12}>
-        <div styleName='error-alert'>
-          <div styleName='error-header'>
-            <button type='button'
-                    styleName='error-close'
-                    onClick={props.onDismiss}
-                    aria-hidden='true'
-                    tabIndex='-1'>
-              <span>&times;</span>
-            </button>
-            <h4 styleName='error-title'>{props.title}</h4>
+class ErrorAlert extends React.PureComponent {
+  render() {
+    return (
+      <Row>
+        <Col lg={12}>
+          <div styleName='error-alert'>
+            <div styleName='error-header'>
+              <button type='button'
+                      styleName='error-close'
+                      onClick={this.props.onDismiss}
+                      aria-hidden='true'
+                      tabIndex='-1'>
+                <span>&times;</span>
+              </button>
+              <h4 styleName='error-title'>{this.props.title}</h4>
+            </div>
+            <div styleName='error-body'>
+              {this.props.children}
+            </div>
           </div>
-          <div styleName='error-body'>
-            {props.children}
-          </div>
-        </div>
-      </Col>
-    </Row>
-  );
+        </Col>
+      </Row>
+    );
+  }
 }
 
 ErrorAlert.propTypes = {
