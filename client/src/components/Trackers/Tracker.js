0d3a139856cc464f68ac2cef0c8e75b1cffc5aba
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-09-07 14:21:38 -0700

    make Trackers/Tracker a PureComponent

--- client/src/components/Trackers/Tracker.js
+++ client/src/components/Trackers/Tracker.js
@@ -7,52 +7,54 @@ import CopyToClipboard from 'react-copy-to-clipboard';
 import { requestDeleteTracker } from 'actions';
 import { userCan } from 'common';
 
-function Tracker(props) {
-  const canEdit = userCan(props.currentUser, 'trackers:write');
+class Tracker extends React.PureComponent {
+  render() {
+    const canEdit = userCan(this.props.currentUser, 'trackers:write');
 
-  const editColumn = canEdit ? (
-    <td style={{ textAlign: 'center' }}>
-      <LinkContainer to={`/trackers/${props.id}`}>
-        <Button bsStyle='primary' bsSize='xsmall' title='edit'>
-          <Glyphicon glyph='edit' />
+    const editColumn = canEdit ? (
+      <td style={{ textAlign: 'center' }}>
+        <LinkContainer to={`/trackers/${this.props.id}`}>
+          <Button bsStyle='primary' bsSize='xsmall' title='edit'>
+            <Glyphicon glyph='edit' />
+          </Button>
+        </LinkContainer>
+      </td>
+    ) : null;
+
+    const removeColumn = canEdit ? (
+      <td style={{ textAlign: 'center' }}>
+        <Button onClick={this.props.onRemove} bsStyle='danger' bsSize='xsmall' title='delete'>
+          <Glyphicon glyph='remove' />
         </Button>
-      </LinkContainer>
-    </td>
-  ) : null;
-
-  const removeColumn = canEdit ? (
-    <td style={{ textAlign: 'center' }}>
-      <Button onClick={props.onRemove} bsStyle='danger' bsSize='xsmall' title='delete'>
-        <Glyphicon glyph='remove' />
-      </Button>
-    </td>
-  ) : null;
-
-  return (
-    <tr>
-      <td><a href={`http://anonym.to/?${props.url}`}>{props.name}</a></td>
-      <td>{props.category}</td>
-
-      <td style={{ textAlign: 'center' }}>
-        <CopyToClipboard text={props.username}>
-          <Button bsStyle='info' bsSize='xsmall' title='copy username'>
-            <Glyphicon glyph='user' />
-          </Button>
-        </CopyToClipboard>
       </td>
+    ) : null;
 
-      <td style={{ textAlign: 'center' }}>
-        <CopyToClipboard text={props.password}>
-          <Button bsStyle='warning' bsSize='xsmall' title='copy password'>
-            <Glyphicon glyph='asterisk' />
-          </Button>
-        </CopyToClipboard>
-      </td>
+    return (
+      <tr>
+        <td><a href={`http://anonym.to/?${this.props.url}`}>{this.props.name}</a></td>
+        <td>{this.props.category}</td>
 
-      {editColumn}
-      {removeColumn}
-    </tr>
-  );
+        <td style={{ textAlign: 'center' }}>
+          <CopyToClipboard text={this.props.username}>
+            <Button bsStyle='info' bsSize='xsmall' title='copy username'>
+              <Glyphicon glyph='user' />
+            </Button>
+          </CopyToClipboard>
+        </td>
+
+        <td style={{ textAlign: 'center' }}>
+          <CopyToClipboard text={this.props.password}>
+            <Button bsStyle='warning' bsSize='xsmall' title='copy password'>
+              <Glyphicon glyph='asterisk' />
+            </Button>
+          </CopyToClipboard>
+        </td>
+
+        {editColumn}
+        {removeColumn}
+      </tr>
+    );
+  }
 }
 
 Tracker.propTypes = {
