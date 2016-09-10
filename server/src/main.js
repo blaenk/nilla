8f0c531aa23e02a7cdea873500a7240fe39f48ca
Jorge Israel Peña <jorge.israel.p@gmail.com>
2016-09-10 01:53:12 -0700

    properly parse port as integers

--- server/src/main.js
+++ server/src/main.js
@@ -10,7 +10,7 @@ const app = server.createServer();
 // eslint-disable-next-line no-magic-numbers
 const { HOST = '0.0.0.0', PORT = 3000, NODE_APP_INSTANCE = 0 } = process.env;
 
-const port = PORT + NODE_APP_INSTANCE;
+const port = parseInt(PORT) + parseInt(NODE_APP_INSTANCE);
 
 const instance = app.listen(port, HOST, () => {
   console.log('Express server listening on port ' + instance.address().port);
