d6ec4a2af23e856cd4e26228dc8499740a68c8fa
Jorge Israel Peña <jorge.israel.p@gmail.com>
2017-10-08 19:44:26 -0700

    build: update webpack 1 → 3

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,13 +1,7 @@
-'use strict';
-
-require('babel-register');
-
 const path = require('path');
 
 const process = require('process');
 const webpack = require('webpack');
-const precss = require('precss');
-const autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
@@ -62,9 +56,6 @@ config.plugins.push(new webpack.DefinePlugin({
   __NODE_ENV__: JSON.stringify(NODE_ENV),
 }));
 
-config.plugins.push(new webpack.optimize.OccurenceOrderPlugin());
-config.plugins.push(new webpack.optimize.DedupePlugin());
-
 if (NODE_ENV === 'production') {
   config.plugins.push(new webpack.optimize.UglifyJsPlugin({
     compressor: {
@@ -85,59 +76,39 @@ config.plugins.push(new ExtractTextPlugin('app.css'));
 
 config.module = {};
 
-config.preLoaders = [];
-
-config.preLoaders.push({
-  test: /\.js$/,
-  loader: 'eslint',
-  include: [src, test],
-});
-
 config.module.loaders = [];
 
 config.module.loaders.push({
   test: /isIterable/,
-  loader: 'imports?Symbol=>false',
+  loader: 'imports-loader?Symbol=>false',
 });
 
 config.module.loaders.push({
   test: /\.js$/,
   include: [src, test],
-  loader: 'babel',
-  query: {
-    presets: ['react', 'es2015'],
-  },
+  use: 'babel-loader',
 });
 
 config.module.loaders.push({
   test: /\.json$/,
-  loader: 'json',
+  loader: 'json-loader',
 });
 
 config.module.loaders.push({
   test: /\.module\.less$/,
-  loader: ExtractTextPlugin.extract(
-    'style?sourceMap',
-    [
-      'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+  use: ExtractTextPlugin.extract({
+    fallback: 'style-loader?sourceMap',
+    use: [
+      'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
       // 'resolve-url',
-      'less?sourceMap',
-    ]
-  ),
-});
-
-config.module.loaders.push({
-  test: /\.css$/,
-  loader: ExtractTextPlugin.extract('style', 'css?sourceMap'),
+      'less-loader?sourceMap',
+    ],
+  }),
 });
 
 config.module.loaders.push({
   test: /\.(eot|woff|woff2|ttf|svg|png|jpe?g|gif)(\?\S*)?$/,
-  loader: 'url?limit=100000&name=[name].[ext]',
+  loader: 'url-loader?limit=100000&name=[name].[ext]',
 });
-
-config.postcss = function() {
-  return [precss, autoprefixer];
-};
 
 module.exports = config;
