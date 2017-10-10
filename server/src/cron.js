890ed1c101420935dcb746fa051a95db8838ad41
Jorge Israel Peña <jorge.israel.p@gmail.com>
2017-10-09 21:43:53 -0700

    fix: use real cron format; alter intervals

--- server/src/cron.js
+++ server/src/cron.js
@@ -4,12 +4,12 @@ const schedule = require('node-schedule');
 
 const tasks = require('./tasks');
 
-const EVERY_10_MINUTES = '10 * * * *';
+const EVERY_5_MINUTES = '*/5 * * * *';
 
 // eslint-disable-next-line no-unused-vars
-const pruneJob = schedule.scheduleJob(EVERY_10_MINUTES, tasks.prune);
+const pruneJob = schedule.scheduleJob(EVERY_5_MINUTES, tasks.prune);
 
-const EVERY_30_MINUTES = '30 * * * *';
+const EVERY_10_MINUTES = '*/10 * * * *';
 
 // eslint-disable-next-line no-unused-vars
-const staleJob = schedule.scheduleJob(EVERY_30_MINUTES, tasks.stale);
+const staleJob = schedule.scheduleJob(EVERY_10_MINUTES, tasks.stale);
